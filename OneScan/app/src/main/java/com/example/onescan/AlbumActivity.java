ca051a41b1c075c0b283e68de8b3565e2b06cdef
y5mei <meiyaowen@gmail.com>
2021-06-12 01:14:44 -0400

    Structure Builded

--- OneScan/app/src/main/java/com/example/onescan/AlbumActivity.java
+++ OneScan/app/src/main/java/com/example/onescan/AlbumActivity.java
@@ -1,4 +1,14 @@
 package com.example.onescan;
 
-public class AlbumActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AlbumActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_album);
+    }
 }
--- OneScan/app/src/main/java/com/example/onescan/MainActivity.java
+++ OneScan/app/src/main/java/com/example/onescan/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.onescan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void gotoAlbum(View v){
+        Intent intent = new Intent(this, AlbumActivity.class);
+        startActivity(intent);
+    }
+
+    public void gotoInstuction(View v){
+        Intent intent = new Intent(this, InstructionActivity.class);
+        startActivity(intent);
+    }
+
+    public void gotoPicture(View v){
+        Intent intent = new Intent(this, PictureActivity.class);
+        startActivity(intent);
     }
 }
