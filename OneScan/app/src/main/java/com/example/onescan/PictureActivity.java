75eed8eeed20da836a62feefda32f4a8b1391e0d
y5mei <meiyaowen@gmail.com>
2021-06-13 18:15:42 -0400

    add image capture

--- OneScan/app/src/main/java/com/example/onescan/PictureActivity.java
+++ OneScan/app/src/main/java/com/example/onescan/PictureActivity.java
@@ -2,13 +2,52 @@ package com.example.onescan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class PictureActivity extends AppCompatActivity {
+//    https://www.youtube.com/watch?v=DPHkhamDoyc&t=198s
+//    https://developer.android.com/training/camera/photobasics
+
+    ImageView imageView;
+    Button button;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_picture);
+
+        imageView = findViewById(R.id.imageView);
+        button = findViewById(R.id.captureButton);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Capture Pressed", Toast.LENGTH_LONG).show();
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                }
+            }
+        });
+
     }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+        }
+    }
+
+
 }
